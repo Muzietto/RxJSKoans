f270b9928b750bc8e386f031f9a55f8e2924fb5b
Muzietto <marco_faustinelli@yahoo.com>
2014-12-11 14:52:19 +0100

    final implementation translator

--- rxjs_tutorial_2010/post_05 - composing callbacks/getToken.php
+++ rxjs_tutorial_2010/post_05 - composing callbacks/getToken.php
@@ -49,76 +49,13 @@ class AccessTokenAuthentication {
             if ($objResponse->error){
                 throw new Exception($objResponse->error_description);
             }
-            return $objResponse->access_token;
+            return $objResponse;
         } catch (Exception $e) {
             echo "Exception-".$e->getMessage();
         }
     }
 }
-/*
- * Class:HTTPTranslator
- * 
- * Processing the translator request.
- */
-Class HTTPTranslator {
-    /*
-     * Create and execute the HTTP CURL request.
-     *
-     * @param string $url        HTTP Url.
-     * @param string $authHeader Authorization Header string.
-     * @param string $postData   Data to post.
-     *
-     * @return string.
-     *
-     */
-    function curlRequest($url, $authHeader, $postData=''){
-        //Initialize the Curl Session.
-        $ch = curl_init();
-        //Set the Curl url.
-        curl_setopt ($ch, CURLOPT_URL, $url);
-        //Set the HTTP HEADER Fields.
-        curl_setopt ($ch, CURLOPT_HTTPHEADER, array($authHeader,"Content-Type: text/xml"));
-        //CURLOPT_RETURNTRANSFER- TRUE to return the transfer as a string of the return value of curl_exec().
-        curl_setopt ($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        //CURLOPT_SSL_VERIFYPEER- Set FALSE to stop cURL from verifying the peer's certificate.
-        curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, False);
-        if($postData) {
-            //Set HTTP POST Request.
-            curl_setopt($ch, CURLOPT_POST, TRUE);
-            //Set data to POST in HTTP "POST" Operation.
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
-        }
-        //Execute the  cURL session.
-        $curlResponse = curl_exec($ch);
-        //Get the Error Code returned by Curl.
-        $curlErrno = curl_errno($ch);
-        if ($curlErrno) {
-            $curlError = curl_error($ch);
-            throw new Exception($curlError);
-        }
-        //Close a cURL session.
-        curl_close($ch);
-        return $curlResponse;
-    }
-    /*
-     * Create Request XML Format.
-     *
-     * @param string $languageCode  Language code
-     *
-     * @return string.
-     */
-    function createReqXML($languageCode) {
-        //Create the Request XML.
-        $requestXml = '<ArrayOfstring xmlns="http://schemas.microsoft.com/2003/10/Serialization/Arrays" xmlns:i="http://www.w3.org/2001/XMLSchema-instance">';
-        if($languageCode) {
-            $requestXml .= "<string>$languageCode</string>";
-        } else {
-            throw new Exception('Language Code is empty.');
-        }
-        $requestXml .= '</ArrayOfstring>';
-        return $requestXml;
-    }
-}
+
 try {
     //Client ID of the application.
     $clientID       = "celermentetrasudandocragganmore";
@@ -135,51 +72,8 @@ try {
     //Get the Access token.
     $accessToken  = $authObj->getTokens($grantType, $scopeUrl, $clientID, $clientSecret, $authUrl);
 
-    echo "<table border=2px>";
-    echo "<tr>";
-    echo "<td><b>TOKEN</b></td>";
-    echo "</tr>";
-    echo "<tr>";
-    echo "<td>" . $accessToken . "</td>";
-    echo "</tr>";
-    echo "</table>";
-
-    //Create the authorization Header string.
-    $authHeader = "Authorization: Bearer ". $accessToken;
-    
-    //Create the Translator Object.
-    $translatorObj = new HTTPTranslator();
-    
-    //Input String.
-    $inputStr = 'Evviva la minestra che salta dalla finestra.';
-    //HTTP Detect Method URL.
-    $detectMethodUrl = "http://api.microsofttranslator.com/V2/Http.svc/Detect?text=".urlencode($inputStr);
-    //Call the curlRequest.
-    $strResponse = $translatorObj->curlRequest($detectMethodUrl, $authHeader);
-    //Interprets a string of XML into an object.
-    $xmlObj = simplexml_load_string($strResponse);
-    foreach((array)$xmlObj[0] as $val){
-        $languageCode = $val;
-    }
-    /*
-     * Get the language Names from languageCodes.
-     */
-    $locale = 'en';
-    $getLanguageNamesurl = "http://api.microsofttranslator.com/V2/Http.svc/GetLanguageNames?locale=$locale";
-    //Create the Request XML format.
-    $requestXml = $translatorObj->createReqXML($languageCode);
-    //Call the curlRequest.
-    $curlResponse = $translatorObj->curlRequest($getLanguageNamesurl, $authHeader, $requestXml);
-    //Interprets a string of XML into an object.
-    $xmlObj = simplexml_load_string($curlResponse);
-    echo "<table border=2px>";
-    echo "<tr>";
-    echo "<td><b>LanguageCodes</b></td><td><b>Language Names</b></td>";
-    echo "</tr>";
-    foreach($xmlObj->string as $language){
-        echo "<tr><td>".$inputStr."</td><td>". $languageCode."(".$language.")"."</td></tr>";
-    }
-    echo "</table>";
+    header('Content-Type: application/json');
+    echo json_encode($accessToken);
     
 } catch (Exception $e) {
     echo "Exception: " . $e->getMessage() . PHP_EOL;
